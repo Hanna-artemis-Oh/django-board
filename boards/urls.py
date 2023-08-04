8d2ebd26196a2764c95923026e92a8e144223b00
Hanna-artemis-Oh <artemiss02@hanmail.net>
2023-08-04 14:44:22 +0900

    complete all required features

--- boards/urls.py
+++ boards/urls.py
@@ -6,6 +6,7 @@ app_name = 'boards'
 urlpatterns = [
     path("", views.index, name='index'),
     path("write/", views.write, name='write'),
+    path("write/success/", views.write_success, name='write_success'),
     path("<int:post_id>/", views.detail, name='detail'),
     path("<int:post_id>/edit/", views.edit, name="edit"),
     path("<int:post_id>/delete/", views.delete, name="delete"),
--- boards/views.py
+++ boards/views.py
@@ -1,18 +1,9 @@
 import json
-
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-
 from .models import Post, Comment
 
 
 def index(request):
-    # author = request.POST['author']
-    # title = request.POST['title']
-    # content = request.POST['content']
-    # saved_post = Post(author=author, title=title, content=content)
-    # saved_post.save()
     post_list = Post.objects.all().order_by('-created_at')
     context = {'post_list': post_list}
     return render(request, 'boards/index.html', context)
@@ -20,6 +11,15 @@ def index(request):
 
 def write(request):
     return render(request, 'boards/write.html')
+
+
+def write_success(request):
+    author = request.POST['author']
+    title = request.POST['title']
+    content = request.POST['content']
+    saved_post = Post(author=author, title=title, content=content)
+    saved_post.save()
+    return redirect('boards:index')
 
 
 def detail(request, post_id):
@@ -34,6 +34,15 @@ def detail(request, post_id):
         post.save()
     except:
         print('No comment posted')
+    try:
+        json_object = json.loads(request.body)
+        like = json_object.get('like')
+        post = get_object_or_404(Post, pk=post_id)
+        if like == 1:
+            post.like += 1
+            post.save()
+    except:
+        print('broken like button')
 
     post = get_object_or_404(Post, pk=post_id)
     context = {'post': post}
@@ -42,7 +51,18 @@ def detail(request, post_id):
 
 def edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    context = {'post': post}
+    try:
+        context = {'post': post}
+        author = request.POST['author']
+        title = request.POST['title']
+        content = request.POST['content']
+        post.author = author
+        post.title = title
+        post.content = content
+        post.save()
+        return redirect('boards:index')
+    except:
+        print('수정 실패')
     return render(request, 'boards/edit.html', context)
 
 
